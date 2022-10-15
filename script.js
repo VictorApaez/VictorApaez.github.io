42ef831aa257379060c7a21d0bcf40ef5a9d8d7f
Victor Apaez <vic.apaez@gmail.com>
2022-10-14 22:46:27 -0400

    hide nav when scrolling down and changed mobile nav hover

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-let nav = document.querySelector(".toggle");
+let burgerIcon = document.querySelector(".material-symbols-outlined");
+let nav = document.querySelector(".nav");
+let links = document.querySelectorAll(".nav__li");
+let header = document.querySelector(".scroll-effect");
 
-addEventListener("click", () => {
+burgerIcon.addEventListener("click", () => {
   nav.classList.toggle("toggle");
+  header.classList.toggle("scroll-effect");
 });
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("toggle");
+  });
+});
+
+let prev = window.pageYOffset;
+
+window.onscroll = function () {
+  let current = window.pageYOffset;
+  let num;
+  prev > current ? (num = "0") : (num = "-60px");
+  document.querySelector(".scroll-effect").style.top = `${num}`;
+  prev = current;
+};
